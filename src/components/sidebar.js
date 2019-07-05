f3dd56d63d4c507fe38ce080481c9f6f0f5a9baa
Harold <hjuarez20@gmail.com>
2019-07-05 11:32:15 -0600

    Enabled activePage

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,21 +5,31 @@ import React from "react"
 import SidebarItem from "./sidebarItem";
 import {Link} from "gatsby";
 
+import _split from "lodash/split";
+import _isEqual from "lodash/isEqual";
+
 // @TODO 
 // Set active class from activePage
 // Remove eslint-disable
 // Add useEffect and update state and expand / collapse elements
 const Sidebar = ({ items, activePage, language }) => {
+  const activePageItems = _split(activePage, '/');
+
   return (
     <>
       <aside className="sidebar sidebar-sticky sidebar-stick-shadow pr-md-5 br-1">
         <ul className="nav nav-sidebar nav-sidebar-hero" data-accordion="true">
           {items && (
             items.map(item => {
+              const linkItems = _split(item.link, '/');
+              const isActive = _isEqual(activePageItems[2], linkItems[0]);
+              const link = `docs/${language}/${item.link}`;
               return (
                 <li key={`${item.link}-li}`} className="nav-item">
-                  <Link className="nav-link active font-size-16" to={`/docs/${language}/${item.link}`}>
-                    {item.title}
+                  <Link className={`nav-link ${isActive && 'active'} font-size-16`} to={`/${link}`}>
+                    {
+                      activePage === link ? <strong>{item.title}</strong> : item.title
+                    }
                     {/* <i className="nav-angle"></i> */}
                   </Link>
                   {item.items && (
--- src/components/sidebarItem.js
+++ src/components/sidebarItem.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Link } from "gatsby";
 
 const SidebarItem = ({ item, activePage, language }) => {
-
   return (
     item.items && (
       item.items.map((item, i) => {
+        const link = `docs/${language}/${item.link}`;
         return (
-          <Link key={i} className="nav-link font-size-14" to={`/docs/${language}/${item.link}`}>{item.title}</Link>
+          <Link key={i} className="nav-link font-size-14" to={`/${link}`}>
+            {
+              activePage === link ? <strong>{item.title}</strong> : item.title
+            }
+            </Link>
         )
       })
     )
