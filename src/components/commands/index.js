05cd9b524c49dd7f73303f562245c32433c9868e
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2019-07-06 02:24:26 -0700

    refactor: add namespace search to autocoplete (#33)

--- src/components/commands/index.js
+++ src/components/commands/index.js
@@ -8,77 +8,58 @@ const Commands = ({ data, language }) => {
   const [search, setSearch] = useState("")
 
   return (
-    <div className="container col-md-12">
+    <div>
       <div className="form-group">
-        {/* <div className="input-group"> */}
-          {/* <div className="input-group-addon">
-            <i className="fa fa-search" />
-          </div> */}
-          {/* <input
-            type="text"
-            id="command-search"
-            className="form-control"
-            placeholder="Search DrupalConsole Commands"
-            onChange={e => setSearch(e.target.value)}
-            value={search}
-          /> */}
           <Autosuggest commands={data} setSearch={setSearch} />
-          {/* <div
-            style={{ background: "#fff; cursor:pointer" }}
-            className="input-group-addon"
-            id="clear-filter"
-            onClick={e => setSearch("")}
-          >
-            <span className="fa fa-times" />
-          </div> */}
-        {/* </div> */}
       </div>
 
-      <table className="table table-commands table-bordered table-striped">
-        <thead>
-          <tr className="d-flex">
-            <th className="col-md-6 col-xl-6">Command</th>
-            <th className="col-md-6 col-xl-6">Usage</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.filter(command => {
-              return search ? command.name === search : true
-              // return command.name.indexOf(search) >= 0
-            })
-            .map((command, i) => {
-              return (
-                <tr className="d-flex" key={i}>
-                  <td className="col-md-6 col-xl-6">
-                    <strong className="command-name">
-                      <Link to={`/docs/${language}/commands/${command.dashed}`}>{command.name}</Link>
-                    </strong>
-                    <br />
-                    <small>{command.description}</small>
-                  </td>
-                  <td className="col-md-6 col-xl-6">
-                    <li className="command-usage">
+      <div className="form-group">
+        <table className="table table-commands table-bordered table-striped">
+          <thead>
+            <tr className="d-flex">
+              <th className="col-md-6 col-xl-6">Command</th>
+              <th className="col-md-6 col-xl-6">Usage</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.filter(command => {
+                // return search ? command.name === search : true
+                return command.name.indexOf(search) >= 0
+              })
+              .map((command, i) => {
+                return (
+                  <tr className="d-flex" key={i}>
+                    <td className="col-md-6 col-xl-6">
+                      <strong className="command-name">
+                        <Link to={`/docs/${language}/commands/${command.dashed}`}>{command.name}</Link>
+                      </strong>
+                      <br />
+                      <small>{command.description}</small>
+                    </td>
+                    <td className="col-md-6 col-xl-6">
+                      <li className="command-usage">
 
-                      <span style={{ whiteSpace: "pre-line" }}>
-                        <small />
-                        <br />
-                        {command.examples && (
-                          command.examples.map((example, i) => {
-                            return (
-                              <div key={i}>
-                                <small>{example.execution}</small>
-                              </div>
-                            )
-                          })
-                        )}
-                      </span>
-                    </li>
-                  </td>
-                </tr>
-              )
-            })}
-        </tbody>
-      </table>
+                        <span style={{ whiteSpace: "pre-line" }}>
+                          <small />
+                          <br />
+                          {command.examples && (
+                            command.examples.map((example, i) => {
+                              return (
+                                <div key={i}>
+                                  <small>{example.execution}</small>
+                                </div>
+                              )
+                            })
+                          )}
+                        </span>
+                      </li>
+                    </td>
+                  </tr>
+                )
+              })}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
