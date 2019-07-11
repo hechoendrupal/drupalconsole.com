a52c60040768271c72aca2e41d40b4f20b4d620d
Harold Juárez <hjuarez20@gmail.com>
2019-07-11 10:44:18 -0600

    Add favicon (#49)

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,10 +62,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/drupal-console-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
