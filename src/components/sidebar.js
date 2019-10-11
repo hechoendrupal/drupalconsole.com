bc6157a9fba8d2dc7accfcd0e2af32fa6e6a3ad7
Jairo Rondon <jimnoname@gmail.com>
2019-10-11 16:16:17 -0500

    [feat] dropdown sidebar for docs

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -9,43 +9,49 @@ import _isEqual from "lodash/isEqual";
 
 // @TODO 
 // Remove eslint-disable
-const Sidebar = ({ items, activePage, language, rtl }) => {
+const Sidebar = ({ items, activePage, language, rtl, toDropDown }) => {
   const activePageItems = _split(activePage, '/');
 
   return (
-    <>
-      <aside className={`sidebar sidebar-sticky sidebar-stick-shadow pr-md-5 br-1 ${rtl && 'rtl'}`}>
-        <ul className="nav nav-sidebar nav-sidebar-hero" data-accordion="true">
-          {items && (
-            items.map(item => {
-              const linkItems = _split(item.link, '/');
-              const isActive = _isEqual(activePageItems[3], linkItems[0]);
-              const link = `/docs/${language}/${item.link}`;
+    <div className={`${toDropDown&&'dropdown'} float-${rtl ? 'right': 'left'}`}>
+      {toDropDown&&
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuDocs" data-flip="true" data-display="static" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Menu
+        </button>}
+      <div className={`${toDropDown?'dropdown-menu dropdown-sidebar  ':''} ${rtl?'dropdown-menu-right':'dropdown-menu-left'}`} aria-labelledby="dropdownMenuDocs">
+        <aside className={`sidebar sidebar-sticky sidebar-stick-shadow ${rtl && 'rtl'}`}>
+          <ul className="nav nav-sidebar nav-sidebar-hero" data-accordion="true">
+            {items && (
+              items.map(item => {
+                const linkItems = _split(item.link, '/');
+                const isActive = _isEqual(activePageItems[3], linkItems[0]);
+                const link = `/docs/${language}/${item.link}`;
 
-              return (
-                <li key={`${item.link}-li}`} className="nav-item">
-                  <Link className={`nav-link ${isActive && 'active'} font-size-16`} to={link}>
-                    {
-                      activePage === link || isActive ? <strong>{item.title}</strong> : item.title
-                    }
-                    <i className="nav-angle"></i>
-                  </Link>
-                  {item.items && (
-                    <div key={`${item.link}-div-item}`} className="nav">
-                        <SidebarItem
-                          item={item}
-                          activePage={activePage}
-                          language={language}
-                        />
-                    </div>
-                  )}
-                </li>
-              )
-            })
-          )}
-        </ul>
-      </aside>
-    </>
+                return (
+                  <li key={`${item.link}-li}`} className="nav-item">
+                    <Link className={`nav-link ${isActive && 'active'} font-size-16`} to={link}>
+                      {
+                        activePage === link || isActive ? <strong>{item.title}</strong> : item.title
+                      }
+                      <i className="nav-angle"></i>
+                    </Link>
+                    {item.items && (
+                      <div key={`${item.link}-div-item}`} className="nav">
+                          <SidebarItem
+                            item={item}
+                            activePage={activePage}
+                            language={language}
+                          />
+                      </div>
+                    )}
+                  </li>
+                )
+              })
+            )}
+          </ul>
+        </aside>
+      </div>
+    </div>
   )
 }
 
