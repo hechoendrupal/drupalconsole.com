92f43ce93e18409f90d381248142d07a51db2f99
Jairo Rondon <jimnoname@gmail.com>
2019-12-02 19:33:04 -0500

    [feat] add FDI

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -26,3 +26,16 @@ firebase.initializeApp(firebaseConfig);
 
 // Initialize Performance Monitoring and get a reference to the service
 const perf = firebase.performance();
+
+// The perfMetrics object is created by the code that goes in <head>.
+perfMetrics.onFirstInputDelay(function(delay, evt) {
+  ga('send', 'event', {
+    eventCategory: 'Perf Metrics',
+    eventAction: 'first-input-delay',
+    eventLabel: evt.type,
+    // Event values must be an integer.
+    eventValue: Math.round(delay),
+    // Exclude this event from bounce rate calculations.
+    nonInteraction: true,
+  });
+});
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,5 +9,8 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
     <script key="jquery" src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>,
     <script key="popper" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js">></script>,
     <script key="bootstrap" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>,
+    <script>
+    !function(n,e){var t,o,i,c=[],f={passive:!0,capture:!0},r=new Date,a="pointerup",u="pointercancel";function p(n,c){t||(t=c,o=n,i=new Date,w(e),s())}function s(){o>=0&&o<i-r&&(c.forEach(function(n){n(o,t)}),c=[])}function l(t){if(t.cancelable){var o=(t.timeStamp>1e12?new Date:performance.now())-t.timeStamp;"pointerdown"==t.type?function(t,o){function i(){p(t,o),r()}function c(){r()}function r(){e(a,i,f),e(u,c,f)}n(a,i,f),n(u,c,f)}(o,t):p(o,t)}}function w(n){["click","mousedown","keydown","touchstart","pointerdown"].forEach(function(e){n(e,l,f)})}w(n),self.perfMetrics=self.perfMetrics||{},self.perfMetrics.onFirstInputDelay=function(n){c.push(n),s()}}(addEventListener,removeEventListener);
+    </script>
   ])
 }
